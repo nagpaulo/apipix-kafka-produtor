4f0630a2ee0d0a7501a311ba155ebf8b86de2d88
Guilherme Nepomuceno de Carvalho <guilhermenepomuceno46@gmail.com>
2023-11-13 14:53:26 -0300

    Corrigindo produtor avro

--- src/main/java/com/alura/pix/config/ProducerKafkaConfig.java
+++ src/main/java/com/alura/pix/config/ProducerKafkaConfig.java
@@ -1,9 +1,7 @@
 package com.alura.pix.config;
 
 import com.alura.pix.avro.PixRecord;
-import com.alura.pix.dto.PixDTO;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,18 +17,19 @@ import java.util.Map;
 @Configuration
 public class ProducerKafkaConfig {
 
-
-    @Value(value = "${spring.kafka.bootstrap-servers:localhost:9092}")
+    @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
+
+    @Value(value = "${spring.kafka.schema-registry}")
+    private String schemaRegistry;
 
     @Bean
     public ProducerFactory<String, PixRecord> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put( "schema.registry.url",
-                "http://stream.seduc.ce.gov.br:8081");
-        configProps.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put("schema.registry.url", schemaRegistry);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
